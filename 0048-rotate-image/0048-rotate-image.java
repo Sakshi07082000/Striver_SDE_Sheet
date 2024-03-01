65a0f4c07c8b733b4431da5a802da0cf561129ed
Sakshi07082000 <110228463+Sakshi07082000@users.noreply.github.com>
2024-03-01 19:06:49 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (87.51%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -15,17 +15,18 @@ class Solution
             }
         }
         
+        //reverse each row of transposed matrix
         for(int i=0; i<n; i++)
         {
             int start=0, end=n-1;
             while(start<end)
-        {
-            int temp=matrix[i][start];
-            matrix[i][start]=matrix[i][end];  
-            matrix[i][end]=temp;
-            start++;
-            end--;
-        }
+            {
+                int temp=matrix[i][start];
+                matrix[i][start]=matrix[i][end];  
+                matrix[i][end]=temp;
+                start++;
+                end--;
+            }
         }
     }
 }
