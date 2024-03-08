62ee215aabf4b7f9df91e7fd620a2e3bc223a98d
Sakshi07082000 <110228463+Sakshi07082000@users.noreply.github.com>
2024-03-08 13:01:31 +0530

    Time: 0 ms (100.00%), Space: 41 MB (6.03%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,7 +1,7 @@
 //Recursive code without memoization
 
 //class Solution{
-//    public int countPaths(int i, int j, int m, int n, int dp[][]){
+//    public int countPaths(int i, int j, int m, int n){
 //        if(i==(m-1) && j==(n-1))
 //            return 1;
 //        if(i>=m || j>=n)
@@ -9,7 +9,7 @@
 //        else
 //            return (countPaths(i+1, j, m, n) + countPaths(i, j+1, m, n));
 //    public int uniquePaths(int m, int n){
-//        return countPaths(0,0, m, n, dp);}}
+//        return countPaths(0,0, m, n);}}
 
 
 
@@ -26,8 +26,8 @@ class Solution
             return 0;
         if(dp[i][j] != 0)   
             return dp[i][j];
-        
-        dp[i][j] = countPaths(i+1, j, m, n, dp) + countPaths(i, j+1, m, n, dp);
+        else
+            dp[i][j] = countPaths(i+1, j, m, n, dp) + countPaths(i, j+1, m, n, dp);
         return dp[i][j];
     }
     
