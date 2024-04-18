2cc53fd321dbd9f54b61f0351716b224fe9329d2
Sakshi07082000 <110228463+Sakshi07082000@users.noreply.github.com>
2024-04-18 14:59:17 +0530

    Time: 2 ms (85.59%), Space: 46.1 MB (45.37%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,25 +1,29 @@
-//Basic brute force approach
+//Two pointer approach
 
 class Solution 
 {
     public void moveZeroes(int[] nums) 
     {
-        int[] temp= new int[nums.length];
-        int j=0;
-        for(int i=0; i<nums.length; i++)
-        {
-            if(nums[i]!=0)
-                temp[j++]=nums[i];
-                
-        }
+        int j=-1;
         for(int i=0; i<nums.length; i++)
         {
             if(nums[i]==0)
-                temp[j++]=nums[i];                
+            {
+                j=i;
+                break;
+            }
         }
-        for(int i=0; i<nums.length; i++)
+        if(j==-1)
+            return;
+        for(int i=j+1; i<nums.length; i++)
         {
-            nums[i]=temp[i];
+            if(nums[i]!=0)
+            {
+                int swap=nums[i];
+                nums[i]=nums[j];
+                nums[j]=swap;
+                j++;
+            }
         }
     }
 }
