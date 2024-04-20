99cae8cca1bda28c67de722c822ccd1c9e618086
Sakshi07082000 <110228463+Sakshi07082000@users.noreply.github.com>
2024-04-20 11:35:42 +0530

    Update 0268-missing-number.java

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -12,3 +12,23 @@ class Solution
         return i;
     }
 }
+
+
+
+//Optimal approach using sum of n natural numbers
+
+class Solution 
+{
+    public int missingNumber(int[] nums) 
+    {
+        int n=nums.length;
+        int sum=n*(n+1)/2;
+        int sum1=0;
+        for(int i=0; i<n; i++)
+        {
+            sum1=sum1+nums[i];
+        }
+        int missno= sum-sum1;
+        return missno;
+    }
+}
