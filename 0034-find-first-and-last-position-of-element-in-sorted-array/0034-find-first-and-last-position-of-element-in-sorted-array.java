84634751f056e1a554ca323397a635e7b2c59f29
Sakshi07082000 <110228463+Sakshi07082000@users.noreply.github.com>
2024-04-29 15:54:12 +0530

    Time: 0 ms (100.00%), Space: 45.5 MB (85.84%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,23 +1,53 @@
-//Brute force approach
+//Binary Search
 
 class Solution 
 {
+    public int FirstOcc(int nums[], int target)
+    {
+        int low=0, high=nums.length-1, first=-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(nums[mid]==target)
+            {
+                first=mid;
+                high=mid-1;
+            }
+            else if(nums[mid]>target)
+                high=mid-1;
+            else
+                low=mid+1;
+        }
+        return first;
+    }
+    public int LastOcc(int nums[], int target)
+    {
+        int low=0, high=nums.length-1, last=-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(nums[mid]==target)
+            {
+                last=mid;
+                low=mid+1;
+            }
+            else if(nums[mid]<target)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return last;
+    }
     public int[] searchRange(int[] nums, int target) 
     {
-        int first=-1, last=-1;
-        for(int i=0; i<nums.length; i++)
+        int first = FirstOcc(nums, target);
+        if(first == -1)
         {
-            if(nums[i]==target)
-            {
-                if(first == -1)
-                    first = i;
-                last=i;
-                
-                if(last != -1 && last < i )
-                    last=i;
-            } 
+            int ans[]={-1,-1};
+            return ans;
         }
-        int ans[]={first, last};
-        return ans;
+        int last= LastOcc(nums, target);
+        int anss[]= {first, last};
+        return anss;
     }
 }
