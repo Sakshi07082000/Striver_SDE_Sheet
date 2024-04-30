e48cda6957f132dfe22676c146b92105e038c696
Sakshi07082000 <110228463+Sakshi07082000@users.noreply.github.com>
2024-04-30 17:52:07 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (85.02%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,16 @@
+//Brute force approach
+
+class Solution 
+{
+    public boolean search(int[] nums, int target) 
+    {
+        for(int i=0; i<nums.length; i++)
+        {
+            if(nums[i] ==target)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
