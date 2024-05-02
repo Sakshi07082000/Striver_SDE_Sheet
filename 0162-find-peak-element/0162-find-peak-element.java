9c11e678672f822fefc4a5044a51a25259fe233e
Sakshi07082000 <110228463+Sakshi07082000@users.noreply.github.com>
2024-05-02 17:32:39 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (5.36%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,18 +1,20 @@
+//Brute force approach
+
 class Solution 
 {
     public int findPeakElement(int[] nums) 
     {
-        if(nums.length==1)
+        if(nums.length==1)             //if array contains one element
             return 0;
         
         for(int i=0; i<nums.length; i++)
         {
-            if(i==0)
+            if(i==0)                   //check for first element
             {
                 if(nums[i]>nums[i+1])
                     return i;
             }
-            else if(i==nums.length-1)
+            else if(i==nums.length-1)  //check for last element
             {
                 if(nums[i]>nums[i-1])
                     return i;
