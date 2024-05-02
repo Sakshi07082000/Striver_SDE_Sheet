7698af799e86e1521d291c857824bec9b5c644e7
Sakshi07082000 <110228463+Sakshi07082000@users.noreply.github.com>
2024-05-02 17:02:51 +0530

    Time: 0 ms (100.00%), Space: 50.3 MB (35.06%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -6,17 +6,20 @@ class Solution
     {
         if(nums.length == 1)                  //if only one element return that element
             return nums[0];
-        if(nums[0]!=nums[1])
+        if(nums[0]!=nums[1])                 //check to remove edge case, for first element
             return nums[0];
-        if(nums[nums.length-1]!=nums[nums.length-2])
+        if(nums[nums.length-1]!=nums[nums.length-2])   //remkove edge case for last element
             return nums[nums.length-1];
         
         int low=1, high=nums.length-2;
         while(low<=high)
         {
             int mid = (low+high)/2;
-            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1])
+            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1])   //check if middle element is single
                 return nums[mid];
+            
+            //check for even odd pairs and eliminate the side having it
+            
             if((mid%2==1 && nums[mid-1]==nums[mid]) || (mid%2==0 && nums[mid]==nums[mid+1]))
                 low = mid + 1;
             else
