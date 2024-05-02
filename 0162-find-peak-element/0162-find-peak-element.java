0096c2b3281b2a4faa89b4c0be9dd7bf8512d857
Sakshi07082000 <110228463+Sakshi07082000@users.noreply.github.com>
2024-05-02 18:08:23 +0530

    Time: 0 ms (100.00%), Space: 42 MB (78.28%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,23 +1,28 @@
-//Brute force approach
+//Binary search approach
 
 class Solution 
 {
     public int findPeakElement(int[] nums) 
     {
-        if(nums.length==1 || nums[0]>nums[1])     
+        if(nums.length==1 || nums[0]>nums[1])     //check for single or first element
             return 0;
-        if(nums[nums.length-1]>nums[nums.length-2])
+        if(nums[nums.length-1]>nums[nums.length-2])  //check for last element
             return nums.length-1;
         
         int low=1, high=nums.length-2;
         while(low<=high)
         {
             int mid = (low+high)/2;
-            if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1])
+            
+            //check if mid is peak
+            if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]) 
                 return mid;
+            
+            //check if mid is on dec or inc curve, in inc curve eliminate left
             else if(nums[mid]>nums[mid+1])
                 high = mid-1;
             else 
+                //eliminate right since mid in dec curve
                 low = mid+1;
         }
         return -1;
