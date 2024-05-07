a5537f7d346b9ffd8ec4edf56ce230634e966c20
Sakshi07082000 <110228463+Sakshi07082000@users.noreply.github.com>
2024-05-08 00:04:55 +0530

    Time: 16 ms (20.67%), Space: 45.1 MB (20.38%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -4,7 +4,7 @@ class Solution
     {
         s = s.toLowerCase();
         s = s.replace(" ","");
-        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        s = s.replaceAll("[^a-zA-Z0-9]", "");  //replace non-alphanumeric characters
         
         int last=s.length()-1, first=0;
         
