5f58fd80f1edce42ff00c53b4b5236cc82bc0c12
Sakshi07082000 <110228463+Sakshi07082000@users.noreply.github.com>
2024-05-13 12:06:17 +0530

    Time: 1 ms (75.44%), Space: 41.5 MB (41.71%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -5,17 +5,18 @@ class Solution
         if (strs == null || strs.length == 0)
             return "";
         
-        Arrays.sort(strs);
-        String first = strs[0];
-        String last = strs[strs.length - 1];
+        Arrays.sort(strs);                        // flight, flow, flower
+        String first = strs[0];                  //at 0th index we have flight
+        String last = strs[strs.length - 1];    //here we have flower
+        
         int c = 0;
-        while(c < first.length())
+        while(c < first.length())        //counter is les than the length of first word
         {
-            if (first.charAt(c) == last.charAt(c))
+            if (first.charAt(c) == last.charAt(c))   //since sorted, compare first and last
                 c++;
             else
                 break;
         }
-        return c == 0 ? "" : first.substring(0, c);
+        return c == 0 ? "" : first.substring(0, c);    //if c=0 return null else substring 
     }
 }
