93cd1f960e3aaad68fae6a6306997bfa38a2d6b9
Sakshi07082000 <110228463+Sakshi07082000@users.noreply.github.com>
2024-05-27 22:25:10 +0530

    Time: 1 ms (100.00%), Space: 41.8 MB (96.33%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,42 @@
+class Solution 
+{
+    public int myAtoi(String s) 
+    {
+        int ans=0;
+        int i=0;
+        int sign=1;
+        int max=Integer.MAX_VALUE;
+        int min=Integer.MIN_VALUE;
+        
+        if(s.length()==0)
+        {
+            return 0;
+        }
+        
+        while(i<s.length() && s.charAt(i)==' ')  //check for leading white spaces
+        {
+            i++;
+        }
+
+        if(i<s.length() && (s.charAt(i)=='-'|| s.charAt(i)=='+')) //check for sign
+        {
+            if(s.charAt(i)=='-')
+            {
+                sign = -1;
+            }
+            i++;
+        }   
+        while(i<s.length() && s.charAt(i)-'0'<=9 && s.charAt(i)-'0'>=0) //check for integer
+        {
+            int digit = s.charAt(i)-'0'; //if integer, save it in digit
+            
+            if(ans>max/10 || (ans==max/10 && digit>max%10)) //check for overflow condition
+            {
+                return (sign == 1) ? max : min;            
+            }
+            ans=ans*10+digit; //if no overflow add to answer
+            i++;
+        }
+        return ans*sign; //return ans with sign
+    }
+}
