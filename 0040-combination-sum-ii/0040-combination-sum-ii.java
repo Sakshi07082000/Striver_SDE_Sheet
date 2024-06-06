073d96a34762833741ef06ed3144e917857e804e
Sakshi07082000 <110228463+Sakshi07082000@users.noreply.github.com>
2024-06-06 15:31:19 +0530

    Time: 3 ms (83.49%), Space: 43.2 MB (32.25%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,33 @@
+class Solution 
+{
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) 
+    {
+        Arrays.sort(candidates);
+        List<List<Integer>> answer = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+        getcombinationII (0, candidates, target, current, answer);
+        return answer;
+    }
+    
+    void getcombinationII(int index, int[] candidates, int target, List<Integer> current, List<List<Integer>> answer)
+    {
+        if(target==0)
+        {
+            answer.add(new ArrayList(current));
+            return;
+        }
+        
+        for(int i=index; i<candidates.length; i++)
+        {
+            //check for repeated elements at different indexes [1,1,1,....]
+            if(i > index && candidates[i] == candidates[i-1])
+                continue;
+            if(candidates[i] > target)
+                break;
+            
+            current.add(candidates[i]);
+            getcombinationII(i+1, candidates, target-candidates[i], current, answer);
+            current.remove(current.size()-1);
+        }
+    }
+}
