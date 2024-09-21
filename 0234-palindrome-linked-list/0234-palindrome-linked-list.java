39b611e4e627e5b28265f6852c2e7b09f551aa0f
Sakshi07082000 <110228463+Sakshi07082000@users.noreply.github.com>
2024-09-22 00:15:46 +0530

    Time: 6 ms (54.73%), Space: 59.9 MB (64.94%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,7 +10,7 @@
  */
 class Solution 
 {
-    public ListNode reverse(ListNode head)
+    public ListNode reverse(ListNode head)       //function to reverse a ll
     {
         ListNode prev = null, temp = head, front;
         
@@ -23,26 +23,29 @@ class Solution
         }
         return prev;
     }
-    public ListNode midelement(ListNode head)
+    
+    public ListNode midelement(ListNode head)       //function to find first mid element of a ll
     {
         ListNode hare = head, turtle = head;
         
-        while(hare.next != null && hare.next.next != null)
+        while(hare.next != null && hare.next.next != null)    /*first mid element is taken 
+                                                             because we reverse from mid.next */
         {
             turtle = turtle.next;
             hare = hare.next.next;
         }
         return turtle;
     }
+    
     public boolean isPalindrome(ListNode head) 
     {
         ListNode mid = midelement(head);
         ListNode firsthalf = head;
         ListNode secondhalf = reverse(mid.next);
         
-        while(secondhalf != null)
+        while(secondhalf != null)     // second half is taken since first half goes from head to null
         {
-            if(firsthalf.val != secondhalf.val)
+            if(firsthalf.val != secondhalf.val)   //compairing val of first and second half
             {
                 return false;
             }
