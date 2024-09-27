1564fbc09684932abe0ab49110594875dfd13d30
Sakshi07082000 <110228463+Sakshi07082000@users.noreply.github.com>
2024-09-27 21:13:24 +0530

    Time: 1 ms (99.89%), Space: 48.3 MB (67.84%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -9,16 +9,41 @@
  *     }
  * }
  */
+
+//Finding difference(d) in length and moving the longer LL by d
+
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode a = headA;
-        ListNode b = headB;
-        while(a != b){
-            if(a == null && b == null) return null;
-            else if(a == null) a = headB;
-            else if(b == null) b = headA;
-            else{a = a.next; b = b.next;}
+        ListNode temp1 = headA, temp2 = headB;
+        int t1 = 0, t2 = 0;
+        while(temp1 != null) {
+            temp1 = temp1.next;
+            t1++;
         }
-        return a;
+        while(temp2 != null) {
+            temp2 = temp2.next;
+            t2++;
+        }
+        int d = Math.abs(t1 - t2);
+        temp1 = headA; 
+        temp2 = headB;
+        while(d != 0) {
+            if(t1 > t2) {
+                temp1 = temp1.next;
+                d--;
+            }
+            else {
+                temp2 = temp2.next;
+                d--;
+            }
+        }
+        while(temp1 != null & temp2 != null)
+        {
+            if(temp1 == temp2)
+                return temp1;
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+        return null;
     }
 }
