759bb799848adefac238bae57ddf24d5dc2ce11d
Sakshi07082000 <110228463+Sakshi07082000@users.noreply.github.com>
2024-09-27 22:14:03 +0530

    Time: 1 ms (99.89%), Space: 48.9 MB (22.34%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -10,40 +10,30 @@
  * }
  */
 
-//Finding difference(d) in length and moving the longer LL by d
+//OPTIMAL APPROACH
 
-public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode temp1 = headA, temp2 = headB;
-        int t1 = 0, t2 = 0;
-        while(temp1 != null) {
-            temp1 = temp1.next;
-            t1++;
-        }
-        while(temp2 != null) {
-            temp2 = temp2.next;
-            t2++;
-        }
-        int d = Math.abs(t1 - t2);
-        temp1 = headA; 
-        temp2 = headB;
-        while(d != 0) {
-            if(t1 > t2) {
-                temp1 = temp1.next;
-                d--;
-            }
-            else {
-                temp2 = temp2.next;
-                d--;
-            }
-        }
-        while(temp1 != null & temp2 != null)
+public class Solution 
+{
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) 
+    {
+        ListNode t1 = headA, t2 = headB;
+        if(headA == null || headB == null)
+            return null;
+        while(t1 != t2)
         {
-            if(temp1 == temp2)
-                return temp1;
-            temp1 = temp1.next;
-            temp2 = temp2.next;
+            t1 = t1.next;
+            t2 = t2.next;
+            
+            /*this alone is covering the case where there is no intersection 
+            and both t1 t2 reach to null*/
+            if(t1 == t2)
+                return t1;
+            
+            if(t1 == null)
+                t1 = headB;
+            if(t2 == null)
+                t2 = headA;
         }
-        return null;
+        return t1;
     }
 }
