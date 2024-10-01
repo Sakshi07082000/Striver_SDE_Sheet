adcfced31343a9881bb4d6e33a19d6f529a23393
Sakshi07082000 <110228463+Sakshi07082000@users.noreply.github.com>
2024-10-02 01:46:02 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (80.15%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution 
+{       
+    public ListNode get_kth_node(ListNode head, int k, int l)
+    {
+        ListNode temp = head;
+        k = k%l;
+        int kth = l - k -1;
+        while(kth>0)
+        {
+            temp = temp.next;
+            kth--;
+        }
+        return temp;
+    }
+    
+    public ListNode rotateRight(ListNode head, int k) 
+    {
+        if(head == null || head.next == null || k == 0) 
+            return head;
+        
+        ListNode tail = head;
+        int len = 1;
+        while(tail.next != null)
+        {
+            tail = tail.next;
+            len++;
+        }
+
+        if(k%len == 0)
+            return head;
+        
+        k = k%len;
+        
+        ListNode newtail = get_kth_node(head, k, len);
+        tail.next = head;
+        head = newtail.next;
+        newtail.next = null;
+        
+        return head;
+    }
+}
