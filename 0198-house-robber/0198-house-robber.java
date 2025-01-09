4173fb461b09b38f455b109d7f733d04b462cbdf
Sakshi07082000 <110228463+Sakshi07082000@users.noreply.github.com>
2025-01-09 17:30:08 +0530

    Time: 0 ms (100%), Space: 41 MB (53.23%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,24 +1,22 @@
-//Memoization approach
+//Tabulation approach
 
 class Solution {
-    public int house_rob(int index, int[] nums, int[] dp) {
-        if (index == 0)
-            return nums[index];
-        if (index < 0)
-            return 0;
-        if(dp[index] != -1)
-            return dp[index];
-
-        int pick = nums[index] + house_rob(index - 2, nums, dp);
-        int notpick = 0 + house_rob(index - 1, nums, dp);
-
-        return dp[index] = Math.max(pick, notpick);
-    }
 
     public int rob(int[] nums) 
     {
         int[] dp = new int[nums.length];
         Arrays.fill(dp,-1);
-        return house_rob(nums.length - 1, nums, dp);
+        dp[0] = nums[0];
+
+        for(int i=1; i<nums.length; i++)
+        {
+            int pick = nums[i];
+            if(i>1)
+                pick = pick + dp[i - 2];
+                
+            int notpick = 0 + dp[i - 1];
+            dp[i] = Math.max(pick, notpick);
+        }
+        return dp[nums.length-1];
     }
 }
