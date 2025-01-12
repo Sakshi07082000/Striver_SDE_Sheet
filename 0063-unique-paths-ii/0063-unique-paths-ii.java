468b59fd00c47735263facc9c32401d6cb1a9d7d
Sakshi07082000 <110228463+Sakshi07082000@users.noreply.github.com>
2025-01-12 17:20:31 +0530

    Time: 1 ms (19.33%), Space: 41.9 MB (35.13%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,4 +1,4 @@
-//Memoization Approach
+//Tabulation Approach
 class Solution 
 {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) 
@@ -6,24 +6,35 @@ class Solution
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
         int[][] dp = new int[m][n];
-        return Paths(m-1, n-1, obstacleGrid, dp);     
-    }
-    public int Paths(int m, int n, int[][] obstacleGrid, int dp[][])
-    {
+
         //when obstacleGrid is null or [[1]]
         if (obstacleGrid == null || obstacleGrid[0][0] == 1) 
             return 0;
-        if(m==0 && n==0)
-            return 1;
-        if(m>=0 && n>=0 && obstacleGrid[m][n] == 1)
-            return 0;
-        if(m<0 || n<0)
-            return 0;
-        if(dp[m][n] != 0)
-            return dp[m][n];
-        
-        dp[m][n] = Paths(m-1, n, obstacleGrid, dp) + Paths(m, n-1, obstacleGrid, dp);
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(i>=0 && j>=0 && obstacleGrid[i][j] == 1)
+                {
+                    dp[i][j] = 0;
+                    continue;
+                }
 
-        return dp[m][n];
+                if(i==0 && j==0)
+                {
+                    dp[i][j] = 1;
+                    continue;
+                }
+
+                int up = 0, left = 0;
+                if(i>0)
+                    up = dp[i-1][j];
+                if(j>0)
+                    left = dp[i][j-1];
+                
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[m-1][n-1];    
     }
 }
