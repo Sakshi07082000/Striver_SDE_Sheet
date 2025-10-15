ec6def857ea4968bb4d0f535ad3339295706263f
Sakshi07082000 <110228463+Sakshi07082000@users.noreply.github.com>
2025-10-15 16:40:27 +0530

    Time: 2 ms (98.98%), Space: 44.5 MB (99.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,54 +1,20 @@
-//Two Pointer Approach
-//This gives TLE
-
-//class Solution {
-//    public int[] twoSum(int[] nums, int target) {        
-//        Arrays.sort(nums);
-//        int arr[]=new int[2];
-//        int left = 0, right= nums.length-1;        
-//        while(left<right) {
-//            int sum = nums[left] + nums[right];
-//            if(sum==target) {
-//                arr[0]=left;
-//                arr[1]=right; }
-//            else if(sum<target)  left++;
-//            else  right--; }
-//        return arr; } }
-
-//Brute Force
 class Solution 
 {
     public int[] twoSum(int[] nums, int target) 
     {
-        int arr[]=new int[2];
+        //map stored key value pairs
+        //since we need to return intex, therefor, numbers in array is key and index is value
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i=0; i<nums.length; i++)
         {
-            for(int j=i+1; j<nums.length; j++)
-            {
-                if(nums[i]+nums[j]==target)
-                {
-                    arr[0]=i;
-                    arr[1]=j;
-                }
-            }
+            int diff = target - nums[i];
+            if(map.containsKey(diff))
+                //map.get will return the value(index) associated with the key(array numbers)
+                return new int[]{map.get(diff), i};
+            
+            //pushing array elements into map along with its index value
+            map.put(nums[i], i);
         }
-        return arr;
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
-
-//Two Pointer Approach
-//This gives TLE
-
-//class Solution {
-//    public int[] twoSum(int[] nums, int target) {        
-//        Arrays.sort(nums);
-//        int arr[]=new int[2];
-//        int left = 0, right= nums.length-1;        
-//        while(left<right) {
-//            int sum = nums[left] + nums[right];
-//            if(sum==target) {
-//                arr[0]=left;
-//                arr[1]=right; }
-//            else if(sum<target)  left++;
-//            else  right--; }
-//        return arr; } }
